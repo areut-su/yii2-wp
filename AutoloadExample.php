6a59d1e5887b4a3c14f2d9c44d95648585e8b1e1
areut-su <areut.su@gmail.com>
2020-04-06 12:17:31 +0700

    generate extention

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace areutYii2Wp;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
